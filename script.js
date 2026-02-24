92a620cea0946c48e9578a2b6e08bd1fccebc470
Programminghero1 <sajjadhossin351@gmail.com>
2026-02-24 23:42:02 +0600

    added script.js

---
+++ script.js
@@ -0,0 +1,416 @@
+let currentStatus = 'all';
+
+// Total Card Number Count
+let totalCardNumber = document.getElementById("total_count");
+let allChildNumber = document.querySelector(".all_card_section");
+totalCardNumber.innerText = allChildNumber.children.length;
+
+// Count Available Jobs
+let jobNumber = document.getElementById("job_count");
+jobNumber.innerText = allChildNumber.children.length;
+
+
+function designOnly(id) {
+  currentStatus = id;
+  const interviewSection = document.querySelector(".interview_section");
+  const initialCard = document.getElementById("initial_interview_card");
+
+  const allButton = document.getElementById("all_btn");
+  const interviewButton = document.getElementById("interview_btn_three");
+  const rejectButton = document.getElementById("reject_btn_three");
+
+  // bg white adding all button
+  allButton.classList.add('bg-white', 'text-gray-500');
+  interviewButton.classList.add('bg-white', 'text-gray-500');
+  rejectButton.classList.add('bg-white', 'text-gray-500');
+
+  // Remove
+  allButton.classList.remove('bg-blue-500', 'text-white');
+  interviewButton.classList.remove('bg-blue-500', 'text-white');
+  rejectButton.classList.remove('bg-blue-500', 'text-white');
+
+  // design only selected button
+  const selectButton = document.getElementById(id);
+  selectButton.classList.add('bg-blue-500', 'text-white');
+  selectButton.classList.remove("bg-white", "text-gray-500");
+
+  const allCardSection = document.querySelector(".all_card_section");
+  const rejectSection = document.querySelector(".reject_section");
+
+  // Clicking hide and show event
+  if (id === 'all_btn') {
+    allCardSection.classList.remove("hidden");
+    interviewSection.classList.add("hidden");
+    rejectSection.classList.add("hidden");
+    initialCard.classList.add("hidden");
+    updateCounts();
+  }
+  else if (id === 'interview_btn_three') {
+    allCardSection.classList.add("hidden");
+    interviewSection.classList.remove("hidden");
+    rejectSection.classList.add("hidden");
+
+    updateCounts();
+    toggleEmptyCard();
+    addingInterview();
+  }
+  else if (id === 'reject_btn_three') {
+    const rejectSection = document.querySelector(".reject_section");
+    const interviewSection = document.querySelector(".interview_section");
+
+    allCardSection.classList.add("hidden");
+    interviewSection.classList.add("hidden");
+    rejectSection.classList.remove("hidden");
+    updateCounts();
+    toggleEmptyCard();
+    renderReject();
+  }
+}
+
+// Creat a function for initial hide and show
+function toggleEmptyCard() {
+  const initialCard = document.getElementById("initial_interview_card");
+
+  if (
+    (currentStatus === 'interview_btn_three' && interviewList.length === 0) ||
+    (currentStatus === 'reject_btn_three' && rejectList.length === 0)
+  ) {
+    initialCard.classList.remove("hidden");
+  } else {
+    initialCard.classList.add("hidden");
+  }
+}
+
+// 2 empty arry for accesssing card element to creat new card
+let interviewList = [];
+let rejectList = [];
+
+// Interview and Reject click event function
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("interview_btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+    // Accessing all card element
+    const companyName = parentNode.querySelector('.company_name').innerText;
+    const skillHave = parentNode.querySelector('.skill').innerText;
+    const extraInfo = parentNode.querySelector('.extra_info').innerText;
+    const clickedButton = parentNode.querySelector('.clicked').innerText;
+    const paragraphsHave = parentNode.querySelector('.paragraphs').innerText;
+
+    const cardItems = {
+      companyName,
+      skillHave,
+      extraInfo,
+      clickedButton: "Interview",
+      paragraphsHave
+    }
+
+    const companyNameExist = interviewList.find(item => item.companyName == cardItems.companyName);
+    if (!companyNameExist) {
+      interviewList.push(cardItems);
+      console.log(interviewList);
+    }
+
+    // Reject list a jodi same companyName wala card thake ta remove hobe
+    rejectList = rejectList.filter(item => item.companyName !== companyName);
+    updateCounts();
+    toggleEmptyCard();
+    updateSingleCardDesign(companyName);
+    if (currentStatus == 'reject_btn_three') {
+      renderReject();
+    }
+  }
+  else if (event.target.classList.contains("reject_btn")) {
+    // console.log(event.target.parentNode);
+    const parentNode = event.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector('.company_name').innerText;
+    const skillHave = parentNode.querySelector('.skill').innerText;
+    const extraInfo = parentNode.querySelector('.extra_info').innerText;
+    const clickedButton = parentNode.querySelector('.clicked').innerText;
+    const paragraphsHave = parentNode.querySelector('.paragraphs').innerText;
+
+
+    const cardItems = {
+      companyName,
+      skillHave,
+      extraInfo,
+      clickedButton: "Rejected",
+      paragraphsHave
+    }
+
+    const companyNameExist = rejectList.find(item => item.companyName == cardItems.companyName);
+    if (!companyNameExist) {
+      rejectList.push(cardItems);
+    }
+
+    // Inteview list a jodi same companyName wala card thake ta remove hobe
+    interviewList = interviewList.filter(item => item.companyName !== companyName);
+    updateCounts();
+    toggleEmptyCard();
+    updateSingleCardDesign(companyName);
+    if (currentStatus == 'interview_btn_three') {
+      addingInterview();
+    }
+  }
+}
+)
+
+// Design toggling . ei puro function colbe jokhon Inteview ba reject k click kora hobe 
+function updateSingleCardDesign(companyName) {
+
+  // Section list
+  const allCards = [
+    ".all_card_section",
+    ".interview_section",
+    ".reject_section"
+  ];
+  // console.log(allCards);
+
+  // Section-wise loop
+  for (let card of allCards) {
+    const section = document.querySelector(card);
+    // if (!section) continue;
+    if (!section) {
+      continue;
+    }
+
+    // Section-er moddhe sob cards
+    const cards = section.querySelectorAll(".all_jobs_section");
+
+    for (let card of cards) {
+      const name = card.querySelector(".company_name").innerText;
+
+      if (name === companyName) {
+        const clickedButton = card.querySelector(".clicked");
+        const interviewBtn = card.querySelector(".interview_btn");
+        const rejectBtn = card.querySelector(".reject_btn");
+
+        // Interview list check
+        const interviewItem = interviewList.find(item => item.companyName === companyName);
+        if (interviewItem) {
+          clickedButton.innerText = "Interview";
+          clickedButton.className =
+            "clicked bg-white text-green-600 border border-green-600 inline-block py-2 px-3 rounded-lg mb-2";
+
+          interviewBtn.classList.add("bg-green-500", "text-white");
+          rejectBtn.classList.remove("bg-red-500", "text-white");
+          continue;
+        }
+
+        // Reject list check
+        const rejectItem = rejectList.find(item => item.companyName === companyName);
+        if (rejectItem) {
+          clickedButton.innerText = "Rejected";
+          clickedButton.className =
+            "clicked bg-white text-red-600 border border-red-600 inline-block py-2 px-3 rounded-lg mb-2";
+
+          rejectBtn.classList.add("bg-red-500", "text-white");
+          interviewBtn.classList.remove("bg-green-500", "text-white");
+          continue;
+        }
+      }
+    }
+  }
+}
+
+// Delet btn handle
+document.addEventListener("click", function (event) {
+  // delete btn access
+  const deleteBtn = event.target.closest(".delete_btn");
+  if (!deleteBtn) return;
+
+  // delete btn er parent theke companyName access
+  const companyName = deleteBtn.closest(".all_jobs_section")
+    .querySelector(".company_name").innerText;
+
+  // jei gulote oi card ti thakte pare ta sections er moddhe array hishabe add kora holo
+  const sections = [
+    document.querySelector(".all_card_section"),
+    document.querySelector(".interview_section"),
+    document.querySelector(".reject_section")
+  ];
+
+  //match kora card remove
+  for (let s = 0; s < sections.length; s++) {
+    const section = sections[s];
+    // jodi section ti DOM a na thake to porer section ta check korbe r thakle nicher code er dike jabe
+    if (!section) continue;
+
+    const cards = section.getElementsByClassName("all_jobs_section"); // live HTMLCollection
+    for (const card of cards) {
+      if (card.querySelector(".company_name").innerText === companyName) {
+        card.remove();
+      }
+    }
+  }
+
+  // List update
+
+  // jehetu remove card korar por o array te history theke jay  tai count vul hote pare . Ei jonno InterviewList notun kore update korte hoy
+  let newInterviewList = [];
+  for (let i = 0; i < interviewList.length; i++) {
+    if (interviewList[i].companyName !== companyName) {
+      newInterviewList.push(interviewList[i]);
+    }
+  }
+  interviewList = newInterviewList;
+
+  let newRejectList = [];
+  for (let i = 0; i < rejectList.length; i++) {
+    if (rejectList[i].companyName !== companyName) {
+      newRejectList.push(rejectList[i]);
+    }
+  }
+  rejectList = newRejectList;
+
+  // Count update
+  const allSection = document.querySelector(".all_card_section");
+  document.getElementById("total_count").innerText = allSection.children.length;
+  document.getElementById("job_count").innerText = allSection.children.length;
+  updateCounts();
+  toggleEmptyCard();
+});
+
+// For count Interview and Reject card number
+function updateCounts() {
+  document.getElementById("interview_count").innerText = interviewList.length;
+  document.getElementById("reject_count").innerText = rejectList.length;
+  const allSection = document.querySelector(".all_card_section");
+  const interviewSection = document.querySelector(".interview_section");
+  const rejectSection = document.querySelector(".reject_section");
+  const jobNumber = document.getElementById("job_count");
+
+  if (currentStatus === 'all_btn') {
+    // All jobs count
+    jobNumber.innerText = allSection.children.length;
+  }
+  else if (currentStatus === 'interview_btn_three') {
+    // Interview section count
+    // jobNumber.innerText = interviewList.length + " of 8";
+    if (interviewList.length == 0) {
+      jobNumber.innerText = "0";
+    }
+    else {
+      jobNumber.innerText = interviewList.length + " of 8";
+    }
+  }
+  else if (currentStatus === 'reject_btn_three') {
+    // Reject section count
+    // jobNumber.innerText = rejectList.length + " of 8";
+    if (rejectList.length == 0) {
+      jobNumber.innerText = "0";
+    }
+    else {
+      jobNumber.innerText = rejectList.length + " of 8";
+    }
+  }
+}
+
+
+// Card emplement function for Interview
+function addingInterview() {
+  const interviewSection = document.querySelector(".interview_section");
+  interviewSection.innerHTML = ``;
+
+  for (const interview of interviewList) {
+    let div = document.createElement('div');
+    div.className = `all_jobs_section flex flex-col lg:flex-row md:flex-row justify-between items-start bg-white mt-4 p-5 rounded-xl shadow`;
+    div.innerHTML = `
+        <!-- left side -->
+        <div class="left_side space-y-5">
+          <!-- part-1 -->
+          <div>
+            <h4 id="mobile_first_corp" class="company_name text-xl font-semibold">${interview.companyName}</h4>
+            <p class="skill text-gray-500">${interview.skillHave}</p>
+          </div>
+          <!-- part-2 -->
+          <div>
+            <p class="extra_info text-gray-500">${interview.extraInfo}</p>
+          </div>
+          <!-- part-3 -->
+          <div>
+            <p class="clicked bg-white text-green-500 border border-green-500 inline-block py-2 px-3 rounded-lg mb-2">${interview.clickedButton}</p>
+            <p class="paragraphs text-gray-600">Build cross-platform mobile applications using React Native. Work on
+              products used by millions of users
+              worldwide.</p>
+          </div>
+          <!-- part-4 -->
+          <div class="btn-select flex gap-3">
+           <button
+              class="interview_btn border bg-green-500 text-white border-green-500 py-2 px-3 rounded-md  hover:cursor-pointer">Interview</button>
+            <button
+              class="reject_btn border border-red-500 py-2 px-3 rounded-md text-red-500 hover:cursor-pointer">Rejected</button>
+            <!-- <button
+              class="border border-red-500 py-2 px-3 rounded-md text-red-500 hover:cursor-pointer lg:hidden sm:block">Delete</button> -->
+          </div>
+        </div>
+
+        <!-- right side -->
+        <div class="right_side mt-4 lg:mt-0">
+          <button class="delete_btn hover:cursor-pointer flex justify-center items-center gap-2
+                 border border-red-500 py-2 px-3 rounded-md text-red-500
+                 lg:border lg:border-gray-200 lg:bg-white lg:text-black lg:px-4 lg:py-2 lg:rounded-full
+                  md:border md:border-gray-200 md:bg-white md:text-black md:px-4 md:py-2 md:rounded-full">
+            <i class="fa-regular fa-trash-can"></i>
+            <span class="lg:hidden md:hidden">Delete</span>
+          </button>
+        </div>
+    `;
+    interviewSection.appendChild(div);
+  }
+}
+
+// Card emplement function for Rejected 
+function renderReject() {
+  const rejectSection = document.querySelector(".reject_section");
+  rejectSection.innerHTML = "";
+
+  for (const reject of rejectList) {
+
+    const div = document.createElement("div");
+
+    div.className = `all_jobs_section flex flex-col lg:flex-row md:flex-row justify-between items-start bg-white mt-4 p-5 rounded-xl shadow`;
+    div.innerHTML = `
+        <!-- left side -->
+        <div class="left_side space-y-5">
+          <!-- part-1 -->
+          <div>
+            <h4 id="mobile_first_corp" class="company_name text-xl font-semibold">${reject.companyName}</h4>
+            <p class="skill text-gray-500">${reject.skillHave}</p>
+          </div>
+          <!-- part-2 -->
+          <div>
+            <p class="extra_info text-gray-500">${reject.extraInfo}</p>
+          </div>
+          <!-- part-3 -->
+          <div>
+            <p class="clicked bg-[#ffffff] text-red-500 border border-red-500 inline-block py-2 px-3 rounded-lg mb-2">${reject.clickedButton}</p>
+            <p class="paragraphs text-gray-600">Build cross-platform mobile applications using React Native. Work on
+              products used by millions of users
+              worldwide.</p>
+          </div>
+          <!-- part-4 -->
+          <div class="btn-select flex gap-3">
+   <button
+              class="interview_btn border border-green-500 text-green-500 py-2 px-3 rounded-md hover:cursor-pointer">Interview</button>
+            <button
+              class="reject_btn border border-red-500 bg-red-500 text-white py-2 px-3 rounded-md hover:cursor-pointer">Rejected</button>
+            <!-- <button
+              class="border border-red-500 py-2 px-3 rounded-md text-red-500 hover:cursor-pointer lg:hidden sm:block">Delete</button> -->
+          </div>
+        </div>
+
+        <!-- right side -->
+        <div class="right_side mt-4 lg:mt-0">
+          <button class="delete_btn hover:cursor-pointer flex justify-center items-center gap-2
+                 border border-red-500 py-2 px-3 rounded-md text-red-500
+                 lg:border lg:border-gray-200 lg:bg-white lg:text-black lg:px-4 lg:py-2 lg:rounded-full
+                  md:border md:border-gray-200 md:bg-white md:text-black md:px-4 md:py-2 md:rounded-full">
+            <i class="fa-regular fa-trash-can"></i>
+            <span class="lg:hidden md:hidden">Delete</span>
+          </button>
+        </div>
+    `;
+
+    rejectSection.appendChild(div);
+  }
+}
